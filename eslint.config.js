3540dfc61f0ee0a2e1e14911d58612d615dc3098
dyamorph <dyamorph22@gmail.com>
2025-07-20 23:07:36 +0300

    add eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -4,8 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default tseslint.config([
+  ...pluginQuery.configs['flat/recommended'],
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
