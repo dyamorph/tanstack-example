af76e67ea30ce26b9f5dc6ad98d724e97e36861f
dyamorph <dyamorph22@gmail.com>
2025-07-21 23:15:36 +0300

    add query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { useEffect } from "react";
 import "./App.css";
-import { client } from "./shared/api/client";
+import {useQuery} from "@tanstack/react-query";
+import {client} from "./shared/api/client.ts";
 
 function App() {
-  useEffect(() => {
-    // fetch("https://musicfun.it-incubator.app/api/1.0/playlists", {
-    //   headers: {
-    //     'api-key': '5613ead4-6ff3-4be1-bfef-c79bb284fb72'
-    //   }
-    // }).then(function(response) {
-    //   return response.json()
-    // }).then(function(data) {
-    //   console.log(data)
-    // })
-    client
-      .GET("/playlists")
-      .then((response) => response.data)
-      .then((data) => console.log(data));
-  }, []);
+  const query = useQuery({
+    queryKey: ['playlists'],
+    queryFn: () => client.GET("/playlists")
+  })
 
   return <>TEST</>;
 }
