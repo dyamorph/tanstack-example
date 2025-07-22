b2153daadf7ac46b54e83e2fcfac22ebf2eda2a1
dyamorph <dyamorph22@gmail.com>
2025-07-22 21:38:29 +0300

    add react router config, folder organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import "./App.css";
 import {useQuery} from "@tanstack/react-query";
 import {client} from "./shared/api/client.ts";
 
 function App() {
+
+  return <>
+    <h1>TEst</h1>
+    <Playlists />
+  </>;
+}
+
+const Playlists = () => {
   const query = useQuery({
     queryKey: ['playlists'],
     queryFn: () => client.GET("/playlists")
   })
 
-  return <>TEST</>;
+  return (
+      <ul>
+        {query.data?.data?.data.map((playlist) => (
+          <li>{playlist.attributes.title}</li>
+        ))}
+      </ul>
+  )
 }
 
 export default App;
---
+++ src/app/entrypoint/main.tsx
@@ -0,0 +1,32 @@
+import { createRoot } from 'react-dom/client'
+import '../styles/index.css'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {routeTree} from "../../routeTree.gen.ts";
+import {createRouter, RouterProvider} from "@tanstack/react-router";
+
+const router = createRouter({ routeTree })
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            refetchOnReconnect: false,
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+        }
+    }
+})
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
+
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
+)
